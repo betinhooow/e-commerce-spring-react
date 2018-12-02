ae359a1ae1f4bdf19d116bc1167a8925403ecfac
Roberto Nobre <robertonobre90@gmail.com>
2018-12-02 11:01:02 -0200

    Updated project structure and added unique email in Client Update

--- backend/src/main/java/com/roberto/curscomc/dto/ClienteDTO.java
+++ backend/src/main/java/com/roberto/curscomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.roberto.curscomc.domain.Cliente;
+import com.roberto.curscomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
--- backend/src/main/java/com/roberto/curscomc/services/validation/ClienteUpdate.java
+++ backend/src/main/java/com/roberto/curscomc/services/validation/ClienteUpdate.java
@@ -4,15 +4,16 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
-@Constraint(validatedBy = ClienteInsertValidator.class)
+@Constraint(validatedBy = ClienteUpdateValidator.class)
 @Target({ ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 
-public @interface ClienteInsert {
-	String message() default "Erro de validação";
+public @interface ClienteUpdate {
+	String message() default "Erro de v	alidação";
 
 	Class<?>[] groups() default {};
 
