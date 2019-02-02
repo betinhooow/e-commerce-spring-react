f3912ed535a693efb1da89093e2cf66349e265c6
Roberto Nobre <robertonobre90@gmail.com>
2019-02-01 22:18:10 -0200

    Criando o profile para teste

---
+++ backend/src/main/java/com/roberto/curscomc/config/TestConfig.java
@@ -0,0 +1,24 @@
+package com.roberto.curscomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.roberto.curscomc.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private DBService DBService;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+			DBService.instantiateTestDatabase();
+		return true;
+	}
+}
