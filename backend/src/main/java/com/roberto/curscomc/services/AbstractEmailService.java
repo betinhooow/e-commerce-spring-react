e21b0602b5b664c4aa07d67441b9eb181c6eba24
RobertoNobre <robertonobre90@gmail.com>
2019-02-06 13:19:09 -0200

    Finalizando envio de email HTML

--- backend/src/main/java/com/roberto/curscomc/services/AbstractEmailService.java
+++ backend/src/main/java/com/roberto/curscomc/services/AbstractEmailService.java
@@ -2,8 +2,16 @@ package com.roberto.curscomc.services;
 
 import java.util.Date;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import com.roberto.curscomc.domain.Pedido;
 
@@ -12,13 +20,19 @@ public abstract class AbstractEmailService implements EmailService {
 	@Value("${default.sender}")
 	private String sender;
 	
+	@Autowired
+	private TemplateEngine templateEngine;
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
 	@Override
 	public void sendOrderConfirmationEmail(Pedido obj) {
-		SimpleMailMessage sm = prepareSimpleMaisMessageFromPedido(obj);
+		SimpleMailMessage sm = prepareSimpleMailMessageFromPedido(obj);
 		sendEmail(sm);
 	}
 
-	protected SimpleMailMessage prepareSimpleMaisMessageFromPedido(Pedido obj) {
+	protected SimpleMailMessage prepareSimpleMailMessageFromPedido(Pedido obj) {
 		SimpleMailMessage sm = new SimpleMailMessage();
 		sm.setTo(obj.getCliente().getEmail());
 		sm.setFrom(sender);
@@ -26,5 +40,35 @@ public abstract class AbstractEmailService implements EmailService {
 		sm.setSentDate(new Date(System.currentTimeMillis()));
 		sm.setText(obj.toString());
 		return sm;
+	}
+	
+	//popula o template html com os dados do pedido
+	protected String htmlFromTemplatePedido(Pedido obj) {
+		Context context = new Context();
+		context.setVariable("pedido", obj);
+		//endereco de onde esta o template  a ser usado, por padrao o thymeleaf busca em resources/templates
+		return templateEngine.process("email/confirmacaoPedido", context);
+	}
+	
+	@Override
+	public void sendOrderConfirmationHtmlEmail(Pedido obj) {
+		try {//trata a exception de que, caso nao de para passar o pedido para o template, envia sem o template mesmo
+			MimeMessage mm = prepareMimeMessageFromPedido(obj);
+			sendHtmlEmail(mm);
+		}catch (MessagingException e) {
+			sendOrderConfirmationEmail(obj);
+		}
+	}
+
+	protected MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException {
+		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+		MimeMessageHelper mmh = new MimeMessageHelper(mimeMessage, true);
+		
+		mmh.setTo(obj.getCliente().getEmail());
+		mmh.setFrom(sender);
+		mmh.setSubject("Pedido confirmado, código: "+ obj.getId());
+		mmh.setSentDate(new Date(System.currentTimeMillis()));
+		mmh.setText(htmlFromTemplatePedido(obj), true);//seta o corpo do email com o template formatado
+		return mimeMessage;
 	}
 }
--- backend/src/main/java/com/roberto/curscomc/services/MockEmailService.java
+++ backend/src/main/java/com/roberto/curscomc/services/MockEmailService.java
@@ -1,6 +1,8 @@
 package com.roberto.curscomc.services;
 
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
@@ -14,6 +16,13 @@ public class MockEmailService extends AbstractEmailService {
 		LOG.info("Simulando envio de email");
 		LOG.info(msg.toString());
 		LOG.info("Email enviado");
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando envio de email com template");
+		LOG.info(msg.toString());
+		LOG.info("Email com template enviado");
 	}
 	
 }
